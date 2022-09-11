cc0604b9005b6ee76c9991422ff2c6d2fa2c9f00
Daniel Leibovitz <danleibovitz@gmail.com>
2022-09-11 20:53:43 +0200

    add defensive programming to weihaz.R

--- R/weihaz.R
+++ R/weihaz.R
@@ -8,14 +8,23 @@
 #' @export
 #'
 #' @examples
-weihaz <- function(x, shape, scale){
+weihaz <- function(
+    scale,
+    shape,
+    x) {
 
+  # Defend against incorrect argument types
   if (class(shape) != "numeric" & class(shape) != "integer") stop("shape parameter must be numeric")
   if (class(scale) != "numeric" & class(scale) != "integer") stop("scale parameter must be numeric")
   if (class(x) != "numeric" & class(x) != "integer") stop("x parameter must be numeric")
+
+  # Defend against incorrect argument dimensions
+
+
+  # Defend against incorrect argument magnitudes
   if (shape <= 0) stop("shape parameter must be positive")
   if (scale <= 0) stop("scale parameter must be positive")
   if (range(x)[1] <= 0) stop("x parameter must all be positive")
 
-  (shape/scale)*(x/scale)^(shape-1)
+  return((shape/scale)*(x/scale)^(shape-1))
 }
