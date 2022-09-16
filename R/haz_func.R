f8007ba8d410badff27a8389f6943fde62ca2599
Daniel Leibovitz <danleibovitz@gmail.com>
2022-09-16 18:10:54 +0200

    remove unecessary commenting

--- R/haz_func.R
+++ R/haz_func.R
@@ -1,6 +1,5 @@
-# define haz_func() function
 
-#' Define a default hazard function
+#' A default hazard function
 #'
 #' @param t
 #' @param x
@@ -19,7 +18,7 @@ haz_func <- function(t, x, betas, b_haz, ncov = 0, ...) {
   # TODO change c(5:(5+ncov)) and all similar structures to calling the actual names in names_bcov, etc.
   # time <- ifelse(t > max(x[["time"]]), data.table::last(x[["time"]]), sapply( t, function(i) min(x[["time"]][x[["time"]] >= i]) ) ) # for vectorized t, get next largest values of time in discrete time sequence of x
   covariates <- x[x[["time"]] == t, c(5:(5+ncov))] # get covariates value, and all covariate values up to ncov. right now, 5 is the index of "treat"
-  if(ncov != 0){ # if there are more than 1 covariate...
+  if(ncov != 0){ # if there is more than 1 covariate...
     return(exp( rowSums(covariates*betas[betas$time == t, c(2:(2+ncov))]) )*b_haz[t]) # calculate exp() of linear dot product. right now, 2 is the index of "treat" in betas matrix
   }
   else{
