4ceb7a35e44b16243fc81d58ca5d14ca2ac537ce
Daniel Leibovitz <danleibovitz@gmail.com>
2022-09-21 07:57:31 +0200

    add diags argument again. Set default to 0.4

--- R/bcov_generator.R
+++ R/bcov_generator.R
@@ -5,6 +5,7 @@
 #' @param middle Mean of multivariate normal, which generates covariates
 #' @param stime Length of follow-up time
 #' @param n Number of patients
+#' @param diags Covariance between all baseline covariate generation distributions
 #'
 #' @return A data.frame with dimension [n*stime]x[num_bvar]
 #' @export
@@ -12,13 +13,13 @@
 #' @examples
 bcov_generator <- function(
     num_bvar,
-# TODO (remove this arg?:)    diags,
+    diags = 0.4,
     middle,
     stime,
     n) {
 
   covariance <- matrix(data = stats::runif(num_bvar^2, 0.1, 0.5), nrow = num_bvar)
-  diag(covariance) <- 0.4
+  diag(covariance) <- diags
   covariance <- t(covariance) %*% covariance # force positive definite
   bcov <- as.data.frame((MASS::mvrnorm(n, rep.int(middle,num_bvar), covariance))) # generate baseline covariates for each patient
   bcov[,1] <- ifelse(bcov[,1] > middle, 1, 0) # cast as a binary, representing some kind of predisposition to death AND switching
