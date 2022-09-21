1e7c005742ec17502b1f8109d8991a327b150522
Daniel Leibovitz <danleibovitz@gmail.com>
2022-09-21 07:57:46 +0200

    add diags argument again.

--- tests/testthat/test-bcov_generator.R
+++ tests/testthat/test-bcov_generator.R
@@ -1,29 +1,29 @@
 
 # Define test arguments
-nb <- nb
-ds <- ds
-md <- md
-st <- st
-en <- en
+nb <- 4
+ds <- 0.5
+md <- 3
+st <- 10
+en <- 8
 
 # test that incorrect argument types throw errors ####
 test_that("Incorrect 'b_haz' argument throws error", {
   expect_error(
-    bcov_generator(num_bvar = nb, middle = md, stime = st, n = en)
+    bcov_generator(num_bvar = nb, diags = ds, middle = md, stime = st, n = en)
   )
 })
 
 # test incorrect argument dimensions throw errors ####
 test_that("Incorrect 'b_haz' argument dimension throws error", {
   expect_error(
-    bcov_generator(num_bvar = nb, middle = md, stime = st, n = en)
+    bcov_generator(num_bvar = nb, diags = ds, middle = md, stime = st, n = en)
   )
 })
 
 # test that results of correct type and dimension are returned ####
 test_that("Correc function call returns correct class", {
   expect_equal(
-    class(bcov_generator(num_bvar = nb, middle = md, stime = st, n = en)),
+    class(bcov_generator(num_bvar = nb, diags = ds, middle = md, stime = st, n = en)),
     "data.frame"
   )
 })
