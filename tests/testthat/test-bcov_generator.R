c4bf03f7fe563f8455620e45a57c581920c12b1d
Daniel Leibovitz <danleibovitz@gmail.com>
2022-09-21 07:33:17 +0200

    add testing structure

--- tests/testthat/test-bcov_generator.R
+++ tests/testthat/test-bcov_generator.R
@@ -1,3 +1,29 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+# Define test arguments
+nb <-
+ds <- ds
+md <- md
+st <- st
+en <- en
+
+# test that incorrect argument types throw errors ####
+test_that("Incorrect 'b_haz' argument throws error", {
+  expect_error(
+    bcov_generator(num_bvar = nb, diags = ds, middle = md, stime = st, n = en)
+  )
+})
+
+# test incorrect argument dimensions throw errors ####
+test_that("Incorrect 'b_haz' argument dimension throws error", {
+  expect_error(
+    bcov_generator(num_bvar = nb, diags = ds, middle = md, stime = st, n = en)
+  )
+})
+
+# test that results of correct type and dimension are returned ####
+test_that("Correc function call returns correct class", {
+  expect_equal(
+    class(bcov_generator(num_bvar = nb, diags = ds, middle = md, stime = st, n = en)),
+    "data.frame"
+  )
 })
