427d09edcd7ac173b02ada46b442d962702732a7
Daniel Leibovitz <danleibovitz@gmail.com>
2022-09-21 07:52:44 +0200

    remove diags argument.

--- tests/testthat/test-bcov_generator.R
+++ tests/testthat/test-bcov_generator.R
@@ -1,6 +1,6 @@
 
 # Define test arguments
-nb <-
+nb <- nb
 ds <- ds
 md <- md
 st <- st
@@ -9,21 +9,21 @@ en <- en
 # test that incorrect argument types throw errors ####
 test_that("Incorrect 'b_haz' argument throws error", {
   expect_error(
-    bcov_generator(num_bvar = nb, diags = ds, middle = md, stime = st, n = en)
+    bcov_generator(num_bvar = nb, middle = md, stime = st, n = en)
   )
 })
 
 # test incorrect argument dimensions throw errors ####
 test_that("Incorrect 'b_haz' argument dimension throws error", {
   expect_error(
-    bcov_generator(num_bvar = nb, diags = ds, middle = md, stime = st, n = en)
+    bcov_generator(num_bvar = nb, middle = md, stime = st, n = en)
   )
 })
 
 # test that results of correct type and dimension are returned ####
 test_that("Correc function call returns correct class", {
   expect_equal(
-    class(bcov_generator(num_bvar = nb, diags = ds, middle = md, stime = st, n = en)),
+    class(bcov_generator(num_bvar = nb, middle = md, stime = st, n = en)),
     "data.frame"
   )
 })
